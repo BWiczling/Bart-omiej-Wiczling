49e2b513bf20aa132d79f4f6d58d957f34f73f70
BWiczling <117576723+BWiczling@users.noreply.github.com>
2022-11-20 17:05:24 +0100

    Add files via upload

--- Lab_4/Zad4_12.c
+++ Lab_4/Zad4_12.c
@@ -2,16 +2,19 @@
 
 
 int main(){
-    int tab[5][5];
+    int n;
+    printf("Podaj wysokosc trojkatu Pascala ");
+    scanf("%d", &n);
+    int tab[n][n];
     tab[0][0]=1;
-    int space=5;
-    for(int i=0;i<5;i++){
+    int space=n;
+    for(int i=0;i<n;i++){
         while(space>i){
             printf("  ");
             space--;
         }
         for(int j=0;j<=i;j++){
-            space=5;
+            space=n;
             if(i<2||j==0||j==i){
                 tab[i][j]=1;
             }
